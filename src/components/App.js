70cb935c7e2e55c55f38f5cddb9a7ae40060c9fe
Vi Tran <contact.vitran@gmail.com>
2022-01-17 20:06:40 +1100

    App scaffolding complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,24 @@
+import { useState } from "react";
 import video from "../data/video.js";
 
+console.log("Here's your data:", video);
+
 function App() {
-  console.log("Here's your data:", video);
+  const [upVotes, setUpVotes] = useState(video.upvotes);
+  const [downVotes, setDownVotes] = useState(video.downvotes);
+  const [hideComments, setHideComments] = useState(false);
+
+  const votesClick = (votes) => {
+    if (votes === upVotes) {
+      setUpVotes(upVotes + 1);
+    } else {
+      setDownVotes(downVotes + 1);
+    }
+  };
+
+  const commentsClick = () => {
+    setHideComments(!hideComments);
+  };
 
   return (
     <div className="App">
@@ -13,6 +30,30 @@ function App() {
         allowfullscreen
         title="Thinking in React"
       />
+      <h1>{video.title}</h1>
+      <p>
+        {video.views} Views | Uploaded {video.createdAt}
+      </p>
+      <button onClick={() => votesClick(upVotes)}>{upVotes} 👍 </button>
+      <button onClick={() => votesClick(downVotes)}>{downVotes} 👎 </button>
+      <br></br>
+      <br></br>
+      <button onClick={commentsClick}>
+        {hideComments ? "Show Comments" : "Hide Comments"}
+      </button>
+      <hr></hr>
+      
+      {hideComments ? (
+        ""
+      ) : (
+        <div>
+          <h2>{video.comments.length} Comments</h2>
+          <strong>{video.comments[0].user}</strong>
+          <p>{video.comments[0].comment}</p>
+          <strong>{video.comments[1].user}</strong>
+          <p>{video.comments[1].comment}</p>
+        </div>
+      )}
     </div>
   );
 }
